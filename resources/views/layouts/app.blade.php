85bd9394364834ca587b12816b29c17f3224c90b
MaxDeffer <nikita.lapin.201248@gmail.com>
2018-02-10 19:54:19 +0200

    Autification com

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,8 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                            <li><a class="nav-link" href="{{ route('auth.login') }}">Login</a></li>
+                            <li><a class="nav-link" href="{{ route('auth.register') }}">Register</a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
