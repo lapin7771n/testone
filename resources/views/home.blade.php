59581495d25dd25b526c6bd55ac7c79df7fb0fa1
MaxDeffer <nikita.lapin.201248@gmail.com>
2018-02-10 23:03:05 +0200

    new login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="section__mainContent">
+        <div class="container">
+            <div class="row d-flex justify-content-between">
+                <div class="col-lg-3 plate__item plate__html">
+                    <a href="#">
+                        <h2 class="h2__item">HTML/CSS</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemAvailable">Available</small>
+                    </a>
+                </div>
+                <div class="col-lg-3 plate__item plate__javaScript">
+                    <a href="#">
+                        <h2 class="h2__item">JavaScript</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemAvailable">Available</small>
+                    </a>
+                </div>
+                <div class="col-lg-3 plate__item plate__c#">
+                    <a href="#">
+                        <h2 class="h2__item">C#</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemAvailable">Available</small>
+                    </a>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-between">
+                <div class="col-lg-3 plate__item plate__ruby">
+                    <a href="#">
+                        <h2 class="h2__item">Ruby</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemAvailable">Available</small>
+                    </a>
+                </div>
+                <div class="col-lg-3 plate__item plate__python">
+                    <a href="#">
+                        <h2 class="h2__item">Python</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemUnavailable">Unavailable</small>
+                    </a>
+                </div>
+                <div class="col-lg-3 plate__item plate__php">
+                    <a href="#">
+                        <h2 class="h2__item">PHP</h2>
+                        <p class="p__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <small class="small__itemUnavailable"> <i class="fa fa-info-circle  fa__item"></i>Unavailable</small>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
